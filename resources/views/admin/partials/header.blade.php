47ab2f4447eea7776ca6d0828edd76b2cb5ef083
s4r44d43 <sararez@live.it>
2024-06-01 10:20:34 +0200

    [add] logout btn and name user

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -2,10 +2,24 @@
   <nav class="navbar bg-body-tertiary">
     <div class="container-fluid">
       <a class="navbar-brand" href="{{ route('home') }}" target="_blank">Vedi il sito</a>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+      <div class="d-flex">
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+
+        <form class="py-2" action="{{ route('logout') }}" method="POST">
+          @csrf
+          <button type="submit" class="ms-3 btn btn-danger">
+            Logout
+            <i class="fa-solid fa-right-from-bracket"></i>
+          </button>
+        </form>
+
+        <p class="mx-3 pt-3">
+          {{ Auth::user()->name }}
+        </p>
+      </div>
     </div>
   </nav>
 </header>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -11,6 +11,9 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <!-- Font Awesome -->
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.css' integrity='sha512-U9Y1sGB3sLIpZm3ePcrKbXVhXlnQNcuwGQJ2WjPjnp6XHqVTdgIlbaDzJXJIAuCTp3y22t+nhI4B88F/5ldjFA==' crossorigin='anonymous'/>
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
@@ -20,7 +23,7 @@
     <body class="font-sans text-gray-900 antialiased">
        
         @include('admin.partials.header')
-        
+
         @yield('content')
 
     </body>
